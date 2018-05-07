a14dacaad4adedbecb09422cef81035a97afd5dd
Sava <me@savamarkovic.com>
2018-05-07 22:49:40 +0200

    custom exception handler

---
+++ resources/views/errors/default.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Whoops.</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato', sans-serif;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 72px;
+                margin-bottom: 40px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <img src="/img/logo.png" style="margin: 0 auto;display: block">
+                <div class="title">There was an error.</div>
+                    <h3>
+                        <a href="/home">Click here to go to homepage.</a>
+                    </h3>
+            </div>
+        </div>
+    </body>
+</html>

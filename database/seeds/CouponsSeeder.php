8a9d7e4486e0fa13a6dd89c6e58babcd2ec3b9dc
Sergei <asvmos@yandex.ru>
2018-07-11 10:26:01 +0300

    CouponsSeeder updated

--- database/seeds/CouponsSeeder.php
+++ database/seeds/CouponsSeeder.php
@@ -13,26 +13,34 @@ class CouponsSeeder extends Seeder
     {
         \Stripe\Stripe::setApiKey(config('services.stripe.secret'));
 
-        $limit = 10;
-        $coupons = App\Models\DiscountCode:: where('is_activated', 0)
-            ->where('code', '!=', '')
-            ->with('plan')
-            ->take($limit)
-            ->get();
+        $limit_per_month = 4;
+        $months_list = [1, 3, 6, 12];
 
-        foreach ($coupons as $coupon) {
-            try {
-                \Stripe\Coupon::create(array(
-                    "id" => $coupon->code,
-                    "name" => $coupon->name,
-                    "percent_off" => 100,
-                    "duration" => "repeating",
-                    "duration_in_months" => $coupon->plan->month,
-                    "max_redemptions" => 1
-                ));
-            } catch (\Exception $e) {
-                echo $coupon->code . ' : ' . $e->getMessage() . "\r\n";
+        foreach ($months_list as $months) {
+
+            $coupons = \DB::table('discount_codes')
+                ->join('plans', 'discount_codes.plan_id', '=', 'plans.id')
+                ->select('code', 'discount_codes.name', 'plans.month as months_amount')
+                ->where('plans.month', '=', $months)
+                ->take($limit_per_month)
+                ->get();
+
+            foreach ($coupons as $coupon) {
+                echo $coupon->code . "-" . $coupon->months_amount . '-' . $coupon->name . "\r\n";
+                try {
+                    \Stripe\Coupon::create(array(
+                        "id" => $coupon->code,
+                        "name" => $coupon->name,
+                        "percent_off" => 100,
+                        "duration" => "repeating",
+                        "duration_in_months" => $coupon->months_amount,
+                        "max_redemptions" => 1
+                    ));
+                } catch (\Exception $e) {
+                    echo $coupon->code . ' : ' . $e->getMessage() . "\r\n";
+                }
             }
+
         }
 
     }
